96edb3ae2355dc120c73b3e21eda3d482fd53f40
Luis Pinillo <lpinillo@educationdynamics.com>
2023-03-20 21:10:32 -0600

    plugin files

---
+++ luisPinillo-assessment/public/wp/wp-content/plugins/modal-form-plugin/modal-form-plugin.php
@@ -0,0 +1,11 @@
+<?php
+//plugin header information
+/*
+Plugin Name: Modal Form Plugin
+Plugin URI: http://www.example.com
+Description: This plugin creates an opt-in modal form
+Version: 1.0
+Author: Luis Hernandez Pinillo 
+Author URI: http://www.example.com 
+License: GPL2
+*/
